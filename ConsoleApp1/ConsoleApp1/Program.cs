fb8210b821b184e4f33dd07b0deeea27107ca16f
tanuj <tanujk9733@gmail.com>
2024-07-31 09:43:40 +0530

    Created order Service with clean architecture.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿string input = Console.ReadLine();
+String[] arr = input.Split(' ');
+int a = Convert.ToInt32(arr[0]);
+int b = Convert.ToInt32(arr[1]);
+int c = Convert.ToInt32(arr[2]);
+
+Console.WriteLine((a + b) / 2 > c ? "YES" : "NO");
